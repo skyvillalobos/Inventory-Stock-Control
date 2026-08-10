edccf67488a43016d25a954b00b1c06bd9e9c56d
Andr3w2 <jesusher615162@gmail.com>
2026-08-10 15:46:49 -0700

    new

--- InventoryAPI/obj/Debug/net10.0/InventoryAPI.AssemblyInfo.cs
+++ InventoryAPI/obj/Debug/net10.0/InventoryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebc4f72a8a7428fcad0cc5015aa412f54c33196")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
